2f083448d5289e7b9b303200de4d681c2c275285
Yousef-Hisham <yhisham191@gmail.com>
2025-02-01 21:58:00 +0200

    Update p.py

--- p.py
+++ p.py
@@ -41,3 +41,26 @@ elif operation =="*":
   print(first_number * second_number)
 else:
   print("error") 
+    
+    # 1- input first number 
+first_number = float(input("enter first number: "))
+
+# 2- input operation
+operation = input("enter operation type: ")
+
+# 3- input second number
+second_number = float(input("enter second number: "))
+
+# 4- print the result
+
+if operation == "+":
+    print(first_number + second_number)
+elif operation == "-":
+    print(first_number - second_number)
+elif operation == "/":
+    print(first_number / second_number)
+elif operation =="*":
+  print(first_number * second_number)
+else:
+  print("error")
+    
